0dae70da0e93ee29c43a837698165fcddeba7423
mdeng1110 <maydeng1110@yahoo.com>
2023-09-19 21:06:34 -0700

    update profiler'

--- profiler/sorting.py
+++ profiler/sorting.py
@@ -1,8 +1,17 @@
 class BubbleSort:
     def __init___(self):
         pass
-    def sort(self, sort_order='ascending'):
-        pass
+    def sort(self, nums, sort_order='ascending'):
+        for i in range(len(nums) - 1):
+            for j in range(len(nums) - i - 1):
+                if sort_order == 'ascending':
+                    if nums[j] > nums[j + 1]:
+                        nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                elif sort_order == 'descending':
+                    if nums[j] < nums[j + 1]:
+                        nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                
+        return nums
 
 
 class SelectionSort:
