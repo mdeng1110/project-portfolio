0c25fe8bd2f1b4b69cd5239669289ed4bff2cf3e
mdeng1110 <maydeng1110@yahoo.com>
2024-02-09 22:31:34 -0800

    updated turtle

--- turtle/main.py
+++ turtle/main.py
@@ -1,7 +1,21 @@
-from turtle import Turtle, Screen
+import turtle as t
 
-tim_the_turtle = Turtle()
-tim_the_turtle.shape("turtle")
-tim_the_turtle.color("red")
-tim_the_turtle.forward(100)
-tim_the_turtle.right(90)
+tim = t.Turtle()
+
+for _ in range(15):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.penup()
+
+# from turtle import Turtle, Screen
+
+# tim_the_turtle = Turtle()
+# # tim_the_turtle.shape("turtle")
+# # tim_the_turtle.color("red")
+# # tim_the_turtle.forward(100)
+# # tim_the_turtle.right(90)
+
+# for _ in range(4):
+#     tim_the_turtle.forward(100)
+#     tim_the_turtle.left(90)
