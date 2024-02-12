89899c8ea21c656e87d2faa9210a7a9ba5f51f99
mdeng1110 <maydeng1110@yahoo.com>
2024-02-12 13:13:28 -0800

    updated hirst_project

--- hirst_project/main.py
+++ hirst_project/main.py
@@ -1,14 +1,63 @@
-import colorgram
+import turtle as turtle_module
+import random
 
-rgb_colors = []
-colors = colorgram.extract('image.jpg', 30)
-for color in colors:
-    r = color.rgb.r
-    g = color.rgb.g
-    b = color.rgb.b
-    new_color = (r, g, b)
-    rgb_colors.append(new_color)
+turtle_module.colormode(255)
+tim = turtle_module.Turtle()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+color_list = [(239, 236, 238), (238, 237, 236), (237, 237, 241), (25, 108, 164), (194, 38, 81), (238, 161, 49), (234, 215, 85), (226, 237, 228), (223, 137, 176), (144, 108, 56), (102, 197, 219), (206, 166, 29), (20, 57, 132), (214, 73, 90), (239, 89, 50), (141, 208, 227), (118, 192, 140), (3, 186, 176), (106, 107, 199), (138, 29, 73), (4, 161, 86), (98, 51, 36), (22, 156, 210), (232, 165, 184), (175, 185, 221), (29, 90, 95), (233, 172, 161), (152, 213, 190), (242, 205, 8), (89, 48, 31)]
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 100
 
-print(rgb_colors)
-    
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
 
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+
+
+
+
+
+
+
+
+screen = turtle_module.Screen()
+screen.exitonclick()
+
+# import turtle as turtle_module
+# import random
+
+# turtle_module.colormode(255)
+# tim = turtle_module.Turtle()
+# tim.speed("fastest")
+# tim.penup()
+# color_list = [(239, 236, 238), (238, 237, 236), (237, 237, 241), (25, 108, 164), (194, 38, 81), (238, 161, 49), (234, 215, 85), (226, 237, 228), (223, 137, 176), (144, 108, 56), (102, 197, 219), (206, 166, 29), (20, 57, 132), (214, 73, 90), (239, 89, 50), (141, 208, 227), (118, 192, 140), (3, 186, 176), (106, 107, 199), (138, 29, 73), (4, 161, 86), (98, 51, 36), (22, 156, 210), (232, 165, 184), (175, 185, 221), (29, 90, 95), (233, 172, 161), (152, 213, 190), (242, 205, 8), (89, 48, 
+# 31)]
+
+# tim.setheading(225)
+# tim.forward(300)
+# tim.setheading(0)
+# number_of_dots = 100
+
+# for dot_count in range(1, number_of_dots + 1):
+#     tim.dot(20, random.choice(color_list))
+#     tim.forward(50)
+
+#     if dot_count % 10 == 0:
+#         tim.setheading(90)
+#         tim.forward(50)
+#         tim.setheading(180)
+#         tim.forward(500)
+
+# # screen = turtle_module.Screen()
+# # screen.exitonclick()
