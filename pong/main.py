70f14b843b50ba68841a45bee6e43584a9a82158
mdeng1110 <maydeng1110@yahoo.com>
2024-03-02 23:16:32 -0800

    update pong

--- pong/main.py
+++ pong/main.py
@@ -4,13 +4,14 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
+screen.tracer(0)
 
 paddle = Turtle()
 paddle.shape("square")
 paddle.color("white")
 paddle.shapesize(stretch_wid=5, stretch_len=1)
 paddle.penup()
-paddle.goto(150, 0)
+paddle.goto(350, 0)
 
 def go_up():
     new_y = paddle.ycor() + 20
@@ -23,5 +24,9 @@ def go_down():
 screen.listen()
 screen.onkey(go_up, "Up")
 screen.onkey(go_down, "Down")
+
+game_is_on = Turtle
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
