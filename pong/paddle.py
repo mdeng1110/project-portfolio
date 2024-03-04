3c1b579b3b3aa582b60d3165f9819d4d74e33fea
mdeng1110 <maydeng1110@yahoo.com>
2024-03-03 22:07:13 -0800

    add paddle

---
+++ pong/paddle.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+
+class Paddle(Turtle):
+
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
+
+    def go_up():
+        new_y = paddle.ycor() + 20
+        paddle.goto(paddle.xcor(), new_y)
+
+    def go_down():
+        new_y = paddle.ycor() - 20
+        paddle.goto(paddle.xcor(), new_y)
