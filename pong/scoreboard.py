b976b0c7de211f15bf4cf5b7659d6834a2aa5a68
mdeng1110 <maydeng1110@yahoo.com>
2024-03-14 21:11:46 -0700

    update scoreboard

--- pong/scoreboard.py
+++ pong/scoreboard.py
@@ -11,4 +11,7 @@ class Scoreboard(Turtle):
         self.r_score = 0
         self.goto(-100, 200)
         self.write(self.l_score, align="center", font=("Courier", 80, "normal"))
+        self.goto(100, 200)
+        self.write(self.r_score, align="center", font=("Courier", 80, "normal"))
+
 
