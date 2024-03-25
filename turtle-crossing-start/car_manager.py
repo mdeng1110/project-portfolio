6fa6cf9736367dc90d81b3df76b506c2dd48d910
mdeng1110 <maydeng1110@yahoo.com>
2024-03-24 21:58:54 -0700

    update create cars method

--- turtle-crossing-start/car_manager.py
+++ turtle-crossing-start/car_manager.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+import random
 
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
@@ -11,9 +12,16 @@ class CarManager:
         all_cars = []
 
     def create_cars(self):
-        new_car = Turtle("square")
-        new_car.shapesize(stretch_wid=2, stretch_len=1)
-        new_car.penup()
-        new_car.color(random.choice(COLORS))    
-        random_y = random.randint(-250, 250) 
-        new_car.goto(300, random_y)   
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))    
+            random_y = random.randint(-250, 250) 
+            new_car.goto(300, random_y)   
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(STARTING_MOVE_DISTANCE)
