3706a6860c815c496b5f2afdaf9622863fced268
mdeng1110 <maydeng1110@yahoo.com>
2024-03-24 21:30:08 -0700

    add create cars method

--- turtle-crossing-start/car_manager.py
+++ turtle-crossing-start/car_manager.py
@@ -1,7 +1,15 @@
+from turtle import turtle
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+    
+    def __init__(self):
+        all_cars = []
+
+    def create_cars(self):
+        new_car = Turtle("square")
+        new_car.shapesize()
