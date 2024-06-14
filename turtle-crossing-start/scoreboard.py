c2fe58d29c3a42a908d2eec7d7598c140f1a63a0
mdeng1110 <maydeng1110@yahoo.com>
2024-06-13 21:38:44 -0700

    update scoreboard and add game over

--- turtle-crossing-start/scoreboard.py
+++ turtle-crossing-start/scoreboard.py
@@ -11,5 +11,17 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.penup()
         self.goto(-200, 200)
-        self.write(f"Level: {self.level}", align="left", font=FONT)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"level: {self.level}", align="left", font=FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write(f"GAME OVER", align="center", font=FONT)
     
