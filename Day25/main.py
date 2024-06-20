95254a38ba64cc8e00b929014442453e6c47da0a
mdeng1110 <maydeng1110@yahoo.com>
2024-06-19 22:09:00 -0700

    update Day 25 project with Pandas and dataframe

--- Day25/main.py
+++ Day25/main.py
@@ -1,18 +1,30 @@
-# with open("weather_data.csv") as data_file:
-#     data = data_file.readlines()
-#     print(data)
-
-# import csv
-
-# with open("weather_data.csv") as data_file:
-#     data = csv.reader(data_file)
-#     temperatures = []
-#     for row in data:
-#         if row[1] != "temp":
-#             temperatures.append(int(row[1]))
-#     print(temperatures)
-
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+# print(type(data))
+# print(data["temp"])
+
+# data_dict = data.to_dict()
+# print(data_dict)
+
+# temp_list = data["temp"].to_list()
+# print(len(temp_list))
+
+# print(data["temp"].mean())
+# print(data["temp"].max())
+
+# #Get Data in Columns
+# print(data["condition"])
+# print(data.condition)
+
+# Get Data in Row
+print(data[data.day == "Monday"])
+print(data[data.temp == data.temp.max()])
+
+#Create a dataframe from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
