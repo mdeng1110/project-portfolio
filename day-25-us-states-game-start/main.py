ff4ca238c7c6538302edb5e8177dded9b8f94adc
mdeng1110 <maydeng1110@yahoo.com>
2024-06-20 21:44:18 -0700

    added guessing part of the USA game

--- day-25-us-states-game-start/main.py
+++ day-25-us-states-game-start/main.py
@@ -1,15 +1,28 @@
 import turtle
+import pandas
+
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-# def get_mouse_click_coor(x,y):
-#     print(x, y)
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
 
-# turtle.onscreenclick(get_mouse_click_coor)
-# turtle.mainloop()
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+    print(answer_state)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-print(answer_state)
+    if answer_state in all_states:
+        print(guessed_states)
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
+
+screen.exitonclick()
