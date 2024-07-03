8b39b5627d26f661bdf4fe00c3c05e2f9913e7f8
mdeng1110 <maydeng1110@yahoo.com>
2024-07-03 12:45:17 -0700

    add mile to kilometer converter

--- Day27/main.py
+++ Day27/main.py
@@ -6,6 +6,7 @@ window.title("My First GUI program")
 window.minsize(width=500, height=300)
 
 my_label = tkinter.Label(text="I am a Label", font=("Arial", "24"))
+my_label.config(text="New Text")
 my_label.pack(side="left")
 
 my_label["text"] = "New Text"
@@ -15,5 +16,12 @@ my_label.config(text="New Text")
 
 # tim = turtle.Turtle()
 # tim.write("Some text")
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack(side="left")
+
+input = Entry(width=10)
+print(input.get())
+input.pack(side="left")
 
 # window.mainloop()
