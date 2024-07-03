ad515d117fc3c911bfe9e2d49e391444ac6e5006
mdeng1110 <maydeng1110@yahoo.com>
2024-07-03 13:03:51 -0700

    updated Mile to Kilometer Converter

--- Day27/mile_to_kilo_converter.py
+++ Day27/mile_to_kilo_converter.py
@@ -1,7 +1,13 @@
 from tkinter import *
 
+def miles_to_km():
+    miles = float(miles_input.get())
+    km = round(miles * 1.689)
+    kilometer_result_label.config(text=f"{km}")
+
 window = Tk()
 window.title("Miles to Kilometer Converter")
+window.config(padx=20, pady=20)
 
 miles_input = Entry()
 miles_input.grid(column=1, row=0)
@@ -18,7 +24,7 @@ kilometer_result_label.grid(column=1, row=1)
 kilometer_label = Label(text="Km")
 kilometer_label.grid(column=2, row=1)
 
-calculate_button = Button(text="Calculate")
+calculate_button = Button(text="Calculate", command=miles_to_km)
 calculate_button.grid(column=1, row=2)
 
 window.mainloop()
