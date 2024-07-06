0f73efc60a20a2498feadae26c1bd2aefc50c869
mdeng1110 <maydeng1110@yahoo.com>
2024-07-05 21:39:11 -0700

    added count down

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -14,16 +14,18 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-# import Time
+def start_timer():
+    count_down(5 * 60)
 
-# count = 5
-# while True:
-#     time.sleep(1)
-#     count -= 1
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+import math
 
 def count_down(count):
-    print(count)
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_min}")
     if count > 0:
         window.after(1000, count_down, count - 1)
 
@@ -43,10 +45,12 @@ title_label.grid(column=1, row=0)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-start_button = Button(text="Start", highlightthickness=0)
+
+
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset", highlightthickness=0)
