4d65f71dcc3da3e6d3073c7b933949d619092a4f
mdeng1110 <maydeng1110@yahoo.com>
2024-07-26 13:10:08 -0700

    add times in Birthday Wisher project

--- Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
+++ Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
@@ -16,6 +16,9 @@ import datetime as dt
 
 now = dt.datetime.now()
 year = now.year
-if year == 2024:
-    print("wear a face mask")
-print(year)
+month = now.month
+day_of_week = now.weekday()
+print(day_of_week)
+
+date_of_birth = dt.datetime(year=1995, month=12, day=15)
+print(date_of_birth)
