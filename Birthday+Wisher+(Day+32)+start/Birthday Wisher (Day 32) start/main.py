f6648b7d0a447ab8705cea0d00b028c7bc6a1dbd
mdeng1110 <maydeng1110@yahoo.com>
2024-07-26 13:05:30 -0700

    add datetime module to Birthday Wisher project

--- Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
+++ Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
@@ -1,10 +1,21 @@
-import smtplib
+# import smtplib
 
-my_email = "butterflyirises@gmail.com"
-password = ""
+# my_email = "butterflyirises@gmail.com"
+# password = ""
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addr="maydeng111@yahoo.com", msg="Hello")
-connection.close()
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(
+#         from_addr=my_email, 
+#         to_addr="maydeng111@yahoo.com", 
+#         msg="Hello")
+# connection.close()
+
+import datetime as dt
+
+now = dt.datetime.now()
+year = now.year
+if year == 2024:
+    print("wear a face mask")
+print(year)
