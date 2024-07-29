cf400d6cc9d2b77369c24c4668005767f814c6c1
mdeng1110 <maydeng1110@yahoo.com>
2024-07-29 16:09:53 -0700

    add sunrise and sunset parameters to Sunrise and Sunset project

--- day-33/main.py
+++ day-33/main.py
@@ -1,4 +1,8 @@
 import requests
+from datetime import datetime
+
+MY_LAT = 51.587351
+MY_LONG = -8.12758
 
 # response = requests.get(url="http://api.open-notify.org/iss-now.json")
 # response.raise_for_status()
@@ -12,5 +16,20 @@ import requests
 
 # print(iss_position)
 
-response = requests.get("https://api.sunrise-sunset.org/json")
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
+data = response.json()
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
+
+print(sunrise.split("T"))
+
+time_now = datetime.now()
+
+print(time_now.hour)
