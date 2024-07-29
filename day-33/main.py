01e2ceb70cdad8a0a4cb3396fef14a00c9964f5d
mdeng1110 <maydeng1110@yahoo.com>
2024-07-29 15:15:54 -0700

    add Sunrise Sunset API

--- day-33/main.py
+++ day-33/main.py
@@ -1,4 +1,16 @@
 import requests
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response)
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+
+# data = response.json()
+
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+
+# iss_position = (longitude, latitude)
+
+# print(iss_position)
+
+response = requests.get("https://api.sunrise-sunset.org/json")
+response.raise_for_status()
