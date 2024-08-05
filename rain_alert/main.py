dbd6aa365f3596132271d0b4cd013b9cd693cfc8
mdeng1110 <maydeng1110@yahoo.com>
2024-08-05 11:38:04 -0700

    add more weather paramters

--- rain_alert/main.py
+++ rain_alert/main.py
@@ -12,7 +12,17 @@ weather_params = {
     "lat": 37.687923, 
     "lon": -122.470207,
     "appid": api_key,
+    "cnt": 4,
 }
 
 response = requests.get(OWM_Endpoint, params=weather_params)
-print(response.json())
+response.raise_for_status()
+weather_data = response.json()
+# print(weather_data["list"][0]["weather"][0]["id"])
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+if will_rain:
+    print("Bring an umbrella.")
