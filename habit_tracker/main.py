37641d75b5c8fe5cead3e318ea134bf8fa8e9168
mdeng1110 <maydeng1110@yahoo.com>
2024-08-07 16:01:39 -0700

    add graph to Habit Tracker project

--- habit_tracker/main.py
+++ habit_tracker/main.py
@@ -1,13 +1,29 @@
 import requests 
 
+USERNAME = "may"
+TOKEN = "skjflsjsfjlsfflsjs"
+
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "skjflsjsfjlsfflsjs",
-    "username": "may",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai",
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config)
+print(response.text)
