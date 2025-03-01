a91560da58e8316e390e91558d21567a26f7bffb
mdeng1110 <maydeng1110@yahoo.com>
2025-02-28 23:20:15 -0800

    added commands to find and select particular elements for the Parsing HTML project

--- bs4-start/main.py
+++ bs4-start/main.py
@@ -5,3 +5,10 @@ with open("website.html") as file:
     contents = file.read()
 
 soup = BeautifulSoup(contents, "html.parser")
+# print(soup.title)
+# print(soup.title.string)
+
+# print(soup.prettify())
+
+all_anchor_tags = soup.find_all(name='a')
+print(all_anchor_tags)
