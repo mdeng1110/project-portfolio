0bf44bf8686aa28d1e4893c7823d01ff39f2d9c8
mdeng1110 <maydeng1110@yahoo.com>
2025-08-14 12:27:45 -0700

    add append method

--- Linked_Lists/doubly_linked_list_c.py
+++ Linked_Lists/doubly_linked_list_c.py
@@ -11,11 +11,24 @@ class DoublyLinkedList:
         self.tail = new_node
         self.length = 1
 
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+
     def print_list(self):
         temp = self.head
         while temp is not None:
             print(temp.value)
             temp = temp.next
 
-my_doubly_linked_list = DoublyLinkedList(7)
+my_doubly_linked_list = DoublyLinkedList(1)
+my_doubly_linked_list.append(2)
 my_doubly_linked_list.print_list()
