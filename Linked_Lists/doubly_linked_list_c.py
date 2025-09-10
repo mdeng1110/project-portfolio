3e6cc57e27fcb7281537e8b25ac07661adb2f5fb
mdeng1110 <maydeng1110@yahoo.com>
2025-09-10 14:15:26 -0700

    add insert method for the Doubly Linked List class

--- Linked_Lists/doubly_linked_list_c.py
+++ Linked_Lists/doubly_linked_list_c.py
@@ -75,6 +75,26 @@ class DoublyLinkedList:
             temp.value = value
             return True
         return False
+    
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        
+        new_node = Node(value)
+        before = self.get(index-1)
+        after = before.next
+
+        new_node.prev = before
+        new_node.next = after
+        before.next = new_node
+        after.prev = new_node
+
+        self.length += 1
+        return True
 
 
 # my_doubly_linked_list = DoublyLinkedList(0)
@@ -88,11 +108,18 @@ class DoublyLinkedList:
 # print(my_doubly_linked_list.get(1))
 # print(my_doubly_linked_list.get(2))
 
-my_doubly_linked_list = DoublyLinkedList(11)
-my_doubly_linked_list.append(3)
-my_doubly_linked_list.append(23)
-my_doubly_linked_list.append(7)
+# my_doubly_linked_list = DoublyLinkedList(11)
+# my_doubly_linked_list.append(3)
+# my_doubly_linked_list.append(23)
+# my_doubly_linked_list.append(7)
 
-my_doubly_linked_list.set_value(1,4)
+# my_doubly_linked_list.set_value(1,4)
+
+# my_doubly_linked_list.print_list()
+
+my_doubly_linked_list = DoublyLinkedList(1)
+my_doubly_linked_list.append(3)
+
+my_doubly_linked_list.insert(1,2)
 
 my_doubly_linked_list.print_list()
