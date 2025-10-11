1b31b7118a6ea5f97a20e91a2e008bc7b682e69e
mdeng1110 <maydeng1110@yahoo.com>
2025-10-10 23:46:35 -0700

    add rotate method

--- practice_problems/practice2.py
+++ practice_problems/practice2.py
@@ -13,8 +13,16 @@ def find_second_largest_sorted(lst):
     sorted_nums = sorted(lst, reverse=True)
     return sorted_nums[1]
 
+def rotate_left(lst, k):
+    if not lst:
+        return []
+    k = k % len(lst)
+    return lst[k:] + lst[:k]
+
 print("Original list:", nums)
 reversed_list = reverse_list(nums)
 print("Reversed list:", reversed_list)
 second_largest = find_second_largest_sorted(nums)
 print("Second Largest:", find_second_largest_sorted(nums))
+rotated_list = rotate_left(nums, 2)
+print("Rotated List:", rotated_list)
