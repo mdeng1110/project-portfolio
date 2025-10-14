233b764e566b774661d225169141efa994ec37c0
mdeng1110 <maydeng1110@yahoo.com>
2025-10-13 22:15:52 -0700

    add balanced parentheses method

--- practice_problems/practice03.py
+++ practice_problems/practice03.py
@@ -1,4 +1,5 @@
 hello = "hello"
+paren_input = "(([]))"
 
 def reverse_str(lst):
     stack = []
@@ -12,7 +13,22 @@ def reverse_str(lst):
 
     return "".join(reversed_chars)
 
+def check_balanced_parens(lst):
+    stack = []
+    mapping = {")": "(", "}": "{", "]": "["} 
+
+    for char in lst:
+        if char in mapping.values():
+            stack.append(char)
+        elif char in mapping.keys():
+            if not stack or stack.pop() != mapping[char]:
+                return False
+    return not stack
+
 
 reversed_string = reverse_str(hello)
 print("Original String:", hello)
 print("Reversed string:", reversed_string)
+print("Original parentheses:", paren_input)
+balance_parens = check_balanced_parens(paren_input)
+print("Balanced Parentheses?:", balance_parens)
