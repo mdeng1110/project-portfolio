57d61338797afb2fb36397df4bc5a3802e3a7930
mdeng1110 <maydeng1110@yahoo.com>
2025-10-16 15:51:40 -0700

    add search method

--- practice_problems/practice03.py
+++ practice_problems/practice03.py
@@ -65,6 +65,26 @@ def factorial(n):
     print("Ending condition met.")
     return 1
 
+def search(searchList, key):
+    mid = int(len(searchList)/2)
+    print("searching midpoint at", str(searchList[mid]))
+
+    if mid == 0:
+        print("Key not found!")
+        return key
+    
+    elif key == searchList[mid]:
+        print("Key found!")
+        return searchList[mid]
+    
+    elif key > searchList[mid]:
+        print("SearchList now contains", searchList[mid:len(searchList)])
+        search(searchList[mid:len(searchList)], key)
+
+    else:
+        print("SearchList now contains", searchList[0:mid])
+        search(searchList[0:mid], key)
+
 
 reversed_string = reverse_str(hello)
 print("Original String:", hello)
@@ -75,3 +95,5 @@ print("Balanced Parentheses?:", balance_parens)
 print("Queue Simulation:")
 simulate_print_queue() 
 print(factorial(5))
+aList= list(range(1,21))
+search(aList, 5)
