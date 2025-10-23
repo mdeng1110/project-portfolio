c22b75f781b59189dc52ba49b68c79b3f0ef8cb4
mdeng1110 <maydeng1110@yahoo.com>
2025-10-22 22:09:55 -0700

    add sum of digits method

--- practice_problems/practice04.py
+++ practice_problems/practice04.py
@@ -3,4 +3,14 @@ def factorial(n):
         return 1
     return n * factorial(n-1)
 
-print(factorial(5))
+def sum_of_digits(n):
+    if n == 0:
+        return 0
+    else:
+        return (n % 10) + sum_of_digits(n // 10)
+
+# testing
+print("factorial result: ", factorial(5))
+number1 = 12345
+result1 = sum_of_digits(number1)
+print(f"The sum of digits of {number1} is: {result1}")
