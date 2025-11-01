2765b5719737ea2ae7dfcba0ae6e97cc7213e35a
mdeng1110 <maydeng1110@yahoo.com>
2025-11-01 12:52:27 -0700

    add Sliding Window Maximum

--- practice_problems/practice05.py
+++ practice_problems/practice05.py
@@ -135,6 +135,28 @@ class TestQueueUsingTwoStacks(unittest.TestCase):
         queue.dequeue()
         self.assertEqual(queue.size(), 0)
 
+def sliding_window_max(nums, k):
+
+# Sliding Window Maximum (Optional)
+
+# Given an array of numbers and a window size k, print the maximum in each sliding window.
+# Hint: Use a deque to keep track of potential maxima efficiently.
+
+    dq = deque()
+    result =[]
+    for i in range(len(nums)):
+        if dq and dq[0] == i - k:
+            dq.popleft()
+
+        while dq and nums[dq[-1]] < nums[i]:
+            dq.pop()
+
+        dq.append(i)
+        if i >= k -1:
+            result.append(nums[dq[0]])
+
+    return result
+
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
 
@@ -145,3 +167,9 @@ print("Balanced Parentheses?:", balance_parens)
 reversed_string = reverse_str(hello)
 print("Original String:", hello)
 print("Reversed string:", reversed_string)
+
+arr = [1, 3, -1, -3, 5, 3, 6, 7]
+k = 3
+output = sliding_window_max(arr, k)
+print("Result of Sliding Window Maximum:", output) 
+# Expected output: [3, 3, 5, 5, 6, 7]
