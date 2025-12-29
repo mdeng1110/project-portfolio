77c35379f1cc145d96aae7e64efc6dea3be3fbc2
mdeng1110 <maydeng1110@yahoo.com>
2025-12-29 13:27:34 -0800

    add contains method

--- practice_problems/practice09.py
+++ practice_problems/practice09.py
@@ -28,11 +28,34 @@ class BinarySearchTree:
                     return True
                 temp = temp.right
 
+    def contains(self, value):
+        temp = self.root
+        while temp is not None:
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                return True
+        return False
+
+
+# my_tree = BinarySearchTree()
+# my_tree.insert(2)
+# my_tree.insert(1)
+# my_tree.insert(3)
+# print(my_tree.root.value)
+# print(my_tree.root.left.value)
+# print(my_tree.root.right.value)
 
 my_tree = BinarySearchTree()
-my_tree.insert(2)
-my_tree.insert(1)
-my_tree.insert(3)
-print(my_tree.root.value)
-print(my_tree.root.left.value)
-print(my_tree.root.right.value)
+my_tree.insert(47)
+my_tree.insert(21)
+my_tree.insert(76)
+my_tree.insert(18)
+my_tree.insert(27)
+my_tree.insert(52)
+my_tree.insert(82)
+
+print(my_tree.contains(27))
+print(my_tree.contains(17))
