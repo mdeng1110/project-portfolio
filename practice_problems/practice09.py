ce0f17924c04eb1564d99f94ab59b9d40cc8e094
mdeng1110 <maydeng1110@yahoo.com>
2025-12-30 13:30:23 -0800

    add Hash Table's get method

--- practice_problems/practice09.py
+++ practice_problems/practice09.py
@@ -38,7 +38,34 @@ class BinarySearchTree:
             else:
                 return True
         return False
+    
+class HashTable:
+    def __init__(self, size = 7):
+        self.data_map = [None] * size
+      
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+        return my_hash  
 
+    def print_table(self):
+        for i, val in enumerate(self.data_map): 
+            print(i, ": ", val)
+    
+    def set_item(self, key, value):
+        index = self.__hash(key)
+        if self.data_map[index] == None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
+    
+    def get_item(self, key):
+        index = self.__hash(key)
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
+        return None
 
 # my_tree = BinarySearchTree()
 # my_tree.insert(2)
@@ -48,14 +75,22 @@ class BinarySearchTree:
 # print(my_tree.root.left.value)
 # print(my_tree.root.right.value)
 
-my_tree = BinarySearchTree()
-my_tree.insert(47)
-my_tree.insert(21)
-my_tree.insert(76)
-my_tree.insert(18)
-my_tree.insert(27)
-my_tree.insert(52)
-my_tree.insert(82)
+# my_tree = BinarySearchTree()
+# my_tree.insert(47)
+# my_tree.insert(21)
+# my_tree.insert(76)
+# my_tree.insert(18)
+# my_tree.insert(27)
+# my_tree.insert(52)
+# my_tree.insert(82)
 
-print(my_tree.contains(27))
-print(my_tree.contains(17))
+# print(my_tree.contains(27))
+# print(my_tree.contains(17))
+
+my_hash_table = HashTable()
+my_hash_table.set_item('bolts', 1400)
+my_hash_table.set_item('washers', 50)
+
+print(my_hash_table.get_item('bolts'))
+print(my_hash_table.get_item('washers'))
+print(my_hash_table.get_item('lumber'))
