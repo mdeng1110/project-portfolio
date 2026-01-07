ad8d38d449c70a3e4a58fd81fe16db6c55921f58
mdeng1110 <maydeng1110@yahoo.com>
2026-01-07 10:40:46 -0800

    add two sum method

--- practice_problems/practice10.py
+++ practice_problems/practice10.py
@@ -1,9 +1,30 @@
-car = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
+# car = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
 
-x = car.items()
+# x = car.items()
 
-print(x)
+# print(x)
+
+
+def twoSum(nums, target):
+    #dictionary method
+    num_container = {}
+    for index, num in enumerate(nums):
+        complement = target - num
+        if complement in num_container:
+            return ([num_container[complement], index])
+        num_container[num] = index
+    return []
+    #brute force method
+    # for i in range(len(nums) - 1):
+    #     for j in range(i + 1, len(nums)):
+    #         if nums[i] + nums[j] == target:
+    #             return [i, j]
+    # return []
+    
+nums = [2,7,11,15]
+target = 9
+print(twoSum(nums, target))
