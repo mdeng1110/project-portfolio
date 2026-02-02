4a7715ba459f4f84292cd481836634cab0ed6cc3
mdeng1110 <maydeng1110@yahoo.com>
2026-02-02 09:35:52 -0800

    add valid parentheses method

--- practice_problems/practice11.py
+++ practice_problems/practice11.py
@@ -1,11 +1,24 @@
-from collections import Counter
+def isValid(s):
+    d = {'(':')','{':'}','[':']'}
+    stack = []
+    for i in s:
+        if i in d:
+            stack.append(i)
+        elif len(stack) == 0 or d[stack.pop()] != i:
+            return False
+    return len(stack) == 0
 
-def firstUniqChar(s):
-    counts = Counter(s)
-    for i in range(len(s)):
-        if counts[s[i]] == 1:
-            return i
-    return -1
+# from collections import Counter
+
+# def firstUniqChar(s):
+#     counts = Counter(s)
+#     for i in range(len(s)):
+#         if counts[s[i]] == 1:
+#             return i
+#     return -1
         
-s = "leetcode"
-print(firstUniqChar(s))
+# s = "leetcode"
+# print(firstUniqChar(s))
+
+s = '()[]{}'
+print(isValid(s))
