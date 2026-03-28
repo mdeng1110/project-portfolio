b44c4e216c218622b454f23e9c7b33c414c187d0
mdeng1110 <maydeng1110@yahoo.com>
2026-03-28 13:25:07 -0700

    add Maximum Product Subarray method

--- practice_problems/practice18.py
+++ practice_problems/practice18.py
@@ -7,6 +7,20 @@ def twoSum(nums, target):
         num_container[num] = index
     return []
 
+
+def maxProduct(nums1):
+    cur_max = cur_min = ans = nums1[0]
+    for x in nums1[1:]:
+        if x < 0:
+            cur_max, cur_min = cur_min, cur_max
+        cur_max = max(x, cur_max*x)
+        cur_min = min(x, cur_min*x)
+        ans = max(ans, cur_max)
+    return ans
+
 nums = [2,7,11,15]
 target = 9
-print(twoSum(nums, target))
+# print(twoSum(nums, target))
+
+nums1 = [2,3,-2,4]
+print(maxProduct(nums1))
