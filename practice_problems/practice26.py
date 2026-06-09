4167642a14a835c5772fc6baed013c0e94c4abd9
mdeng1110 <maydeng1110@yahoo.com>
2026-06-08 21:36:06 -0700

    add find duplicates method

--- practice_problems/practice26.py
+++ practice_problems/practice26.py
@@ -9,6 +9,19 @@ def count_occurrences(nums):
     return result
 
 nums = [1,1,2,3,3,3]
+# Print Count Occurrences
 print(count_occurrences(nums))
 
 # Practice Problem #2: Find Duplicates
+def find_duplicates(nums):
+    seen = set()
+    duplicates = set()
+    for num in nums:
+        if num in seen:
+            duplicates.add(num)
+        else:
+            seen.add(num)
+    return list(duplicates)
+
+# Print Find Duplicates
+print(find_duplicates(nums))
