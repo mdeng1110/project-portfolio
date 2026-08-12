e7d0886bf07057098d268bfbf7d4696b51bf4263
mdeng1110 <maydeng1110@yahoo.com>
2026-08-12 16:05:05 -0700

    add using selenium to scrape a different piece of data

--- day48/main.py
+++ day48/main.py
@@ -1,13 +1,49 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
+# Deprecated - no longer needed
+chrome_driver_path = "/Users/philippmuellauer/Development/chromedriver"
+
+# keeps chrome open
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.amazon.com/Instant-Pot-Plus-60-Programmable/dp/B01NBKTPTS/?th=1")
 
-price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole")
-price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
-print(f"The price is {price_dollar.text}.{price_cents.text}")
-#driver.close()
-driver.quit()
+# driver = webdriver.Chrome(executable_path=chrome_driver_path)
+# driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=chrome_options)
+
+def test_eight_components():
+    driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+    title = driver.title
+    assert title == "Web form"
+    driver.implicitly_wait(0.5)
+    text_box = driver.find_element(by=By.NAME, value="my-text")
+    submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+    text_box.send_keys("Selenium")
+    submit_button.click()
+    message = driver.find_element(by=By.ID, value="message")
+    value = message.text
+    assert value == "Received!"
+
+    # Closes Chrome
+    # driver.quit()
+    driver.close()
+
+
+test_eight_components()
+
+
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_experimental_option("detach", True)
+# driver = webdriver.Chrome(options=chrome_options)
+# driver.get("https://www.amazon.com/Instant-Pot-Plus-60-Programmable/dp/B01NBKTPTS/?th=1")
+
+# price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole")
+# price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
+# print(f"The price is {price_dollar.text}.{price_cents.text}")
+# #driver.close()
+# driver.quit()
